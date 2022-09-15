e1c3e6279d24daa5847867754dd1a108cc8c3bb2
"frenchkebab" <"tubowangin@gmail.com">
2022-09-15 19:46:18 +0900

    [#1] Updates to v0.8 code

--- contracts/3-Forcefully-send-ether/EtherGamePrevented.sol
+++ contracts/3-Forcefully-send-ether/EtherGamePrevented.sol
@@ -1,33 +1,14 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.10;
-
 contract EtherGame {
-    uint256 public targetAmount = 7 ether;
+    uint public targetAmount = 3 ether;
+    uint public balance;
     address public winner;
-    uint256 public balance;
 
     function deposit() public payable {
         require(msg.value == 1 ether, "You can only send 1 Ether");
 
-        /*
-            attackers can still forcefully send ethers,
-            but they won't be able to update the state variable.
-            (unless they play the game by calling deposit function)
-        */
         balance += msg.value;
-        // uint256 balance = address(this).balance;
+        require(balance <= targetAmount, "Game is over");
 
-        /*
-            If the current balance of ether stored in this  
-            greater than 7 ether, then the game is over.
-            After 7 ether, no one is able to send ether
-        */
-        require(balance <= targetAmount, "Game is Over");
-
-        /*
-            If the balance of ether stored in this contract
-            exceeds 7 ether, no one will be able to claim the reward 
-        */
         if (balance == targetAmount) {
             winner = msg.sender;
         }
@@ -36,17 +17,7 @@ contract EtherGame {
     function claimReward() public {
         require(msg.sender == winner, "Not winner");
 
-        (bool sent,) = msg.sender.call{value: address(this).balance}("");
+        (bool sent, ) = msg.sender.call{value: balance}("");
         require(sent, "Failed to send Ether");
-    }
-
-    function getBalance() public view returns(uint256) {
-        return address(this).balance;
-    }
-}
-
-contract Attack {
-    function attack(address payable target) public payable {
-        selfdestruct(target);
     }
 }
